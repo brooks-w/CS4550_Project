fa50577bc6a74e801cf077385a4bdd04821eb44b
brooks-w <brooks.w@northeastern.edu>
2024-03-20 16:47:21 -0400

    Add basic account information

---
+++ src/Account/PublicUserInfo/index.tsx
@@ -0,0 +1,29 @@
+function PublicUserInfo() {
+    return (
+        <>
+        <div className="container text-center">
+        <div className="row align-items-start">
+        <div className="col">
+            <h1>Public User Info</h1>
+            <h2>User Information</h2>
+        <ul>
+            <li>Name: William Brooks</li>
+            <li>Username: brooks-w</li>
+            <li>Email: brooks.w@northeastern.edu</li>
+        </ul>
+        <h2>Quick Facts</h2>
+        <ul>
+            <li>Favorite Artist: The Beatles</li>
+            <li>Favorite Song: 20</li>
+        </ul>
+        </div>
+        <div className="col">
+            <img src="https://i.imgur.com/2d8J3tj.jpg" alt="William Brooks" width="200" height="200"/>
+        </div>
+        </div>
+        </div>
+        </>
+    );
+}
+
+export default PublicUserInfo;
